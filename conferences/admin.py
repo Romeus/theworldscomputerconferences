080d458ab02dbd84be5e239a718c6a31ddd16b4e
Romeus <romeusmeister@gmail.com>
2015-03-29 16:56:12 +0300

    Register all models in admin

--- conferences/admin.py
+++ conferences/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from conferences.models import ConferenceType, Category, Place, Conference, Feedback
+
+admin.site.register(ConferenceType)
+admin.site.register(Category)
+admin.site.register(Place)
+admin.site.register(Conference)
+admin.site.register(Feedback)
