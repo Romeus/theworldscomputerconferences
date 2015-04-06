15e92cfb89c51c89145e0870afb294714bb6b87e
Romeus <romeusmeister@gmail.com>
2015-04-06 13:16:41 +0300

    Include conference application urls

--- theworldscomputerconferences/urls.py
+++ theworldscomputerconferences/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'theworldscomputerconferences.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^conferences/', include('conferences.urls')),
 )
