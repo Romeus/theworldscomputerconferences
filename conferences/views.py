f387eb771c5dc45f57f7e281988aec8e8fd83471
Romeus <romeusmeister@gmail.com>
2015-04-14 22:10:17 +0300

    Pass request object to templates

--- conferences/views.py
+++ conferences/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 
 
 def map(request):
-    context_dict = {}
+    context_dict = {'request': request}
     return render(request, 'conferences/map.html', context_dict)
 
 
 def about(request):
-    context_dict = {}
+    context_dict = {'request': request}
     return render(request, 'conferences/about.html', context_dict)
 
 
 def all(request):
-    context_dict = {}
+    context_dict = {'request': request}
     return render(request, 'conferences/all.html', context_dict)
 
 
 def conference(request, conference_name_slug):
-    context_dict = {}
+    context_dict = {'request': request}
     return render(request, 'conferences/conference.html', context_dict)
 
 
 def feedback(request):
-    context_dict = {}
+    context_dict = {'request': request}
     return render(request, 'conferences/feedback.html', context_dict)
