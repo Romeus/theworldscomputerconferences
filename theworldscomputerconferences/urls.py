0a0f8fc189f985b8e02f813188e9c5cc9cdab402
Romeus <romeusmeister@gmail.com>
2015-04-15 17:09:04 +0300

    Rebase urls from "/conference/" to "/"

--- theworldscomputerconferences/urls.py
+++ theworldscomputerconferences/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^conferences/', include('conferences.urls')),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'', include('conferences.urls')),)
